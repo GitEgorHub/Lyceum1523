1b689be9154f1e58e0fa5e8a04777c1fd4c1934c
Egor <65305629+TryKuhn@users.noreply.github.com>
2022-11-14 03:31:15 +0300

    Update insertionSort.cpp

--- squareSortings/insertionSort.cpp
+++ squareSortings/insertionSort.cpp
@@ -1,4 +1,4 @@
-void insertion_sort(vector<int>& a) {
+void insertion_sort(vector<int> &a) {
     for (int i = 0; i < a.size(); i++) {
         int cur = i;
         while (cur > 0 && a[cur - 1] > a[cur]) {
